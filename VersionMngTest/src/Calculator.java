7d214c69312d42c5e837f47a4e65505e17c4eff1
USER <USER@USER18-PC>
2017-10-19 18:59:14 +0900

    집에서 했어요

--- VersionMngTest/src/Calculator.java
+++ VersionMngTest/src/Calculator.java
@@ -1,4 +1,5 @@
 
+//집에서 작업
 public class Calculator {
 	public int sum(int num1,int num2) {
 		return num1+ num2;
@@ -6,6 +7,9 @@ public class Calculator {
 	
 	public int substraction(int num1,int num2) {
 		return num1 -num2;
+	}
+	public int astrisk(int num1,int num2) {
+		return num1 * num2;
 	}
 	
 }
